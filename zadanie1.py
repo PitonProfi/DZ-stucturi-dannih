b23a88c60d755b8b83b650225d45b627ac6c8f37
Olga <imotama2@gmail.com>
2024-11-22 18:44:43 +0300

    DZ stucturi dannih

---
+++ zadanie1.py
@@ -0,0 +1,17 @@
+#zadanie 1
+print(9**0.5*5)
+
+#zadanie2
+print(9.99>9.98 and 1000!=1000.1)
+
+#zadanie3
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#zadanie4
+print ('123.456')
+print (type(float('123.456')))
+print (123.456*10)
+print(int (1234.56))
+print(1234/10)
